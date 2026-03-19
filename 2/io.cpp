f10d6ecab3b1c1af950080f01c4ff4f351f15d32
Jaeyeon Chae <yachae1107@kentech.ac.kr>
2026-03-19 01:39:19 +0000

    all

---
+++ 2/io.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+
+using namespace std;
+int main(int arge, char *argv[])
+{
+	int x{};
+	cout << "Enter a number: " ;
+	cin >> x;
+	cout << "You entered " << x << endl;
+	return 0;
+}
